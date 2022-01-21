93c76a359c998df2bc3d096a6f29e7166af6de1d
austindailor <53894735+austindailor@users.noreply.github.com>
2022-01-21 09:59:00 +0100

    pag404

--- resources/views/noacesso.blade.php
+++ resources/views/noacesso.blade.php
@@ -342,7 +342,7 @@ DRIBBBLE: https://dribbble.com/shots/4330167-404-Page-Lost-In-Space
         </div>
 
         <div class="objects">
-            <img class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px">
+            <img class="object_rocket" src="" width="40px">
             <div class="earth-moon">
                 <img class="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px">
                 <img class="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px">
